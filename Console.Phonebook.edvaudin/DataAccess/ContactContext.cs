00e073486a76cb047bda2c6b23e2e9e1d9285273
Edward Vaudin <edward@vaudin.net>
2023-01-21 19:36:12 +0000

    Transferred project to CodeReview

---
+++ Console.Phonebook.edvaudin/DataAccess/ContactContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Phonebook.Models;
+
+namespace Phonebook.DataAccess;
+
+public class ContactContext : DbContext
+{
+    public DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source=(LocalDb)\\MyLocalDb;Initial Catalog=phonebook;Integrated Security=True");
+    }
+}
