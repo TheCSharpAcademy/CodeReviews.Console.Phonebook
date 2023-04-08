c27a0f2217932c3fc997498d3fb34eb843a3bbb2
Hugo Delautre <97909616+Gautatyr@users.noreply.github.com>
2023-04-08 11:23:39 +0200

    Add Gautatyr finished project

---
+++ Gautatyr.PhoneBook/PhoneBook/Models/PhoneBookContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PhoneBook.Models;
+using System.Configuration;
+
+namespace PhoneBook;
+
+public class PhoneBookContext : DbContext
+{
+    string dbString = ConfigurationManager.AppSettings.Get("ConnectionString");
+
+    public DbSet<Contact> Contacts { get; set; }
+
+    public string DbPath { get; }
+
+    public PhoneBookContext()
+    {
+        DbPath = ConfigurationManager.AppSettings.Get("DbFilePath");
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlServer(dbString);
+}
