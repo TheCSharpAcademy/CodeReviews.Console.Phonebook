b57ce2c6f5a2f892eb88c7ad68393ef655cdee3c
heartingNinja <neilfosteronly@gmail.com>
2023-05-16 07:53:12 +0700

    PhoneBook App with ability to send emails (#6)
    * PhoneBook App with ability to send emails
    
    * Fixed Quality Issues

---
+++ PhoneBook.heartingNinja/PhoneBook/PhoneBook/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+
+namespace PhoneBook
+{
+    public class DataContext : DbContext
+    {
+
+        public DataContext(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+
+        public virtual DbSet<UserPermission> UserPermissions { get; set; }
+    }
+}
---
+++ PhoneBook.heartingNinja/PhoneBook/PhoneBook/User.cs
@@ -0,0 +1,17 @@
+﻿namespace PhoneBook
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
