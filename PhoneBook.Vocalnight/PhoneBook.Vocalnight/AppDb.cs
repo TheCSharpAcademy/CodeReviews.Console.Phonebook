d1306341edead8c69240b78346bc6cbe5052bd7b
Vocal <vocalnight@protonmail.com>
2023-06-11 18:27:12 -0300

    Finished project

---
+++ PhoneBook.Vocalnight/PhoneBook.Vocalnight/AppDb.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    public class AppDb : DbContext
+    {
+        public DbSet<PhoneBook> PhoneBook { get; set; }
+
+        private const string ConnectionString =
+            @"Server=(localdb)\mssqllocaldb;
+            Database=PhoneBookDatabase;
+            Trusted_Connection=True";
+
+        protected override void OnConfiguring( DbContextOptionsBuilder optionsBuilder )
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+
+}
---
+++ PhoneBook.Vocalnight/PhoneBook.Vocalnight/PhoneBook.cs
@@ -0,0 +1,12 @@
+﻿namespace PhoneBook
+{
+    public class PhoneBook
+    {
+        public int PhoneBookId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public string Category { get; set; }
+    }
+}
