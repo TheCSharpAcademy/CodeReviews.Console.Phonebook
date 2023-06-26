bb6a34e6d4707817dd0de686ed1ed617e41ff016
LucianoNicolasArrieta <63777474+LucianoNicolasArrieta@users.noreply.github.com>
2023-06-25 21:41:54 -0300

    Phone Book App project finished (#9)
    * principal requirements done
    
    * Added 'send emails' requirement
    
    * added UserInput and finished sending emails challenge
    
    * Added categories
    
    * removed unused 'using ...'

---
+++ LucianoNicolasArrieta.PhoneBook/LucianoNicolasArrieta.PhoneBook/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LucianoNicolasArrieta.PhoneBook.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<Contact> Contacts { get; set; }
+
+        public Category(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ LucianoNicolasArrieta.PhoneBook/LucianoNicolasArrieta.PhoneBook/Program.cs
@@ -0,0 +1,10 @@
+﻿using LucianoNicolasArrieta.PhoneBook;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Menu menu = new Menu();
+        menu.CategoryMenu();
+    }
+}
