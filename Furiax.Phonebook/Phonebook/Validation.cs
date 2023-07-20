75336d553db45822e5b80df75b554de6643f85aa
furiax <carlmalfliet@proximus.be>
2023-07-20 14:35:59 +0200

    created validation functions on phonenumber and emailaddress

---
+++ Furiax.Phonebook/Phonebook/Validation.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Mail;
+
+namespace Phonebook
+{
+	internal class Validation
+	{
+		static bool IsValidPhoneNumber(string phoneNumber)
+		{
+            foreach (char c in phoneNumber)
+			{
+				if (!char.IsDigit(c))
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+		static bool IsValidEmail(string email)
+		{
+			try
+			{
+				MailAddress mailAddress = new MailAddress(email);
+					return true;
+			}
+			catch (FormatException)
+			{
+				return false;
+			}
+		}
+
+	}
+}
