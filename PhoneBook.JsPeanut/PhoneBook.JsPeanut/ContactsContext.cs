c297eb3cc699b2553f0ab59a0f23863367e7d70b
JsPeanut <cacahuatearg223@outlook.com>
2023-07-22 09:11:14 -0300

    Codacy issues fix

--- PhoneBook.JsPeanut/PhoneBook.JsPeanut/ContactsContext.cs
+++ PhoneBook.JsPeanut/PhoneBook.JsPeanut/ContactsContext.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PhoneBook.JsPeanut.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PhoneBook.JsPeanut
 {
