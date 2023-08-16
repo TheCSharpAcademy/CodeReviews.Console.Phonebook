9b4bb5777373cb3f8697b409b17c52f91c464e44
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-16 14:13:20 +0200

    feature[user interfaces]: create menu options enum

---
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/User Interface/MenuOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Phonebook.MartinL_no.UserInterface;
+
+public enum MenuOptions
+{
+    AddContact,
+    DeleteContact,
+    UpdateContact,
+    ViewContact,
+    ViewAllContacts,
+    Quit
+}
+
