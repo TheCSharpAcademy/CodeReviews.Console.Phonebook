ef9be161bcc4fc22da64a4a177448ad559a0afac
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-16 12:42:10 +0200

    feature[contact controller]: create get contacts method

--- Phonebook.MartinL-no/Phonebook.MartinL-no/ContactController.cs
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/ContactController.cs
@@ -9,4 +9,13 @@ internal class ContactController
 
         db.SaveChanges();
     }
+
+    public static List<Contact> GetContacts()
+    {
+        using var db = new ContactsContext();
+
+        var contacts = db.Contacts.ToList();
+
+        return contacts;
+    }
 }
--- Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
@@ -1,3 +1,6 @@
 ﻿using Phonebook.MartinL_no;
 
-ContactController.AddContact("John Doe", "123456");
+//ContactController.AddContact("John Doe", "123456");
+var contacts = ContactController.GetContacts();
+
+Console.WriteLine();
