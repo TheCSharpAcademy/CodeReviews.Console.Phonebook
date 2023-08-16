8c74b65e2bfbdbdcef8d7f1ad26607babca0bc61
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-16 12:34:16 +0200

    feature[contact controller]: create add contact method

---
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/ContactController.cs
@@ -0,0 +1,12 @@
+﻿namespace Phonebook.MartinL_no;
+
+internal class ContactController
+{
+    public static void AddContact(string name, string phoneNumber)
+    {
+        using var db = new ContactsContext();
+        db.Add(new Contact { Name = name, PhoneNumber = phoneNumber });
+
+        db.SaveChanges();
+    }
+}
--- Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
@@ -1,3 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Phonebook.MartinL_no;
 
+ContactController.AddContact("John Doe", "123456");
