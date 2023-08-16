897fb784136547507e0344d2813f849d71738dbb
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-16 19:07:22 +0200

    feature[contact controller]:  add ability to see table with no contacts

--- Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
@@ -1,12 +1,3 @@
-﻿using Phonebook.MartinL_no;
+﻿using Phonebook.MartinL_no.UserInterface;
 
-ContactController.AddContact("John Doe", "123456");
-var contacts = ContactController.GetContacts();
-//var contact = ContactController.GetContactById(1);
-//ContactController.DeleteContact(1);
-
-var contact = contacts[0];
-contact.PhoneNumber = "98765";
-ContactController.UpdateContact(contacts[0]);
-
-Console.WriteLine();
+UserInterface.ShowMenu();
---
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/User Interface/UserInterface.cs
@@ -0,0 +1,88 @@
+﻿using Spectre.Console;
+
+namespace Phonebook.MartinL_no.UserInterface;
+
+static internal class UserInterface
+{
+	static public void ShowMenu()
+	{
+        while (true)
+        {
+		    var option = AnsiConsole.Prompt(
+			    new SelectionPrompt<MenuOptions>()
+			    .Title("What would you like to do:")
+			    .AddChoices(
+				    MenuOptions.AddContact,
+				    MenuOptions.DeleteContact,
+				    MenuOptions.UpdateContact,
+				    MenuOptions.ViewAllContacts,
+				    MenuOptions.ViewContact));
+
+		    switch (option)
+		    {
+			    case MenuOptions.AddContact:
+				    AddContact();
+                    break;
+			    case MenuOptions.DeleteContact:
+				    DeleteContact();
+                    break;
+                case MenuOptions.UpdateContact:
+				    UpdateContact();
+                    break;
+                case MenuOptions.ViewAllContacts:
+				    ViewAllContacts();
+                    break;
+                case MenuOptions.ViewContact:
+				    ViewContact();
+                    break;
+            }
+        }
+	}
+
+    private static void ViewContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void ViewAllContacts()
+    {
+        var contacts = ContactController.GetContacts();
+
+        ShowContactTable(contacts);
+    }
+
+    private static void UpdateContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    private static void ShowContactTable(List<Contact> contacts)
+    {
+        var table = new Table();
+        table.AddColumn("Id");
+        table.AddColumn("Name");
+        table.AddColumn("Phone Number");
+
+        foreach (var contact in contacts)
+        {
+            table.AddRow(contact.Id.ToString(), contact.Name, contact.PhoneNumber);
+        }
+
+        AnsiConsole.Write(table);
+
+        Console.WriteLine("Enter any key to continue");
+        Console.ReadKey();
+        Console.Clear();
+    }
+}
+
