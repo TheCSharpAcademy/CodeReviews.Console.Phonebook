2339170680e5fbabe6cb05591b32240b83baac81
Martin <102468994+MartinL-no@users.noreply.github.com>
2023-08-16 20:08:00 +0200

    refactor: move main menu to Program.cs

--- Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
+++ Phonebook.MartinL-no/Phonebook.MartinL-no/Program.cs
@@ -1,3 +1,38 @@
-﻿using Phonebook.MartinL_no.UserInterface;
+﻿using Phonebook.MartinL_no.Controllers;
+using Phonebook.MartinL_no.Services;
+using Phonebook.MartinL_no.UserInterface;
+using Spectre.Console;
 
-UserInterface.ShowMenu();
+while (true)
+{
+    var option = AnsiConsole.Prompt(
+        new SelectionPrompt<MenuOptions>()
+        .Title("What would you like to do:")
+        .AddChoices(
+            MenuOptions.AddContact,
+            MenuOptions.DeleteContact,
+            MenuOptions.UpdateContact,
+            MenuOptions.ViewAllContacts,
+            MenuOptions.ViewContact));
+
+    switch (option)
+    {
+        case MenuOptions.AddContact:
+            //ContactController.AddContact();
+            break;
+        case MenuOptions.DeleteContact:
+            //ContactController.DeleteContact();
+            break;
+        case MenuOptions.UpdateContact:
+            //ContactController.UpdateContact();
+            break;
+        case MenuOptions.ViewAllContacts:
+            var contacts = ContactController.GetContacts();
+            UserInterface.ShowContacts(contacts);
+            break;
+        case MenuOptions.ViewContact:
+            var contact = ContactService.GetContactOptionInput();
+            UserInterface.ShowContact(contact);
+            break;
+    }
+}
