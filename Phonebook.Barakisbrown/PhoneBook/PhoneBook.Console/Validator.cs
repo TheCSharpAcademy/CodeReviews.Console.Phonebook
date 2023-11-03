641a17e1c198ead2a8c9b377c05b91c9fc329312
Matthew J Brown <matthew@lokislayer.com>
2023-11-03 01:46:12 -0500

    Inital Commit.

---
+++ Phonebook.Barakisbrown/PhoneBook/PhoneBook.Console/Validator.cs
@@ -0,0 +1,35 @@
+﻿namespace PhoneBook.Console;
+
+using System.Net.Mail;
+
+public class Validator
+{
+    public static bool IsValidEmail(string email)
+    {
+        if (string.IsNullOrEmpty(email)) return false;
+
+        try
+        {
+            var addr = new MailAddress(email);
+            return true;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+
+    public static bool IsValidPhone(string phone)
+    {
+        var phoneNumberUtil = PhoneNumbers.PhoneNumberUtil.GetInstance();
+        try
+        {
+            PhoneNumbers.PhoneNumber number = phoneNumberUtil.Parse(phone, "US");
+            return phoneNumberUtil.IsValidNumber(number);
+        }
+        catch (PhoneNumbers.NumberParseException _)
+        {
+            return false;
+        }
+    }
+}
