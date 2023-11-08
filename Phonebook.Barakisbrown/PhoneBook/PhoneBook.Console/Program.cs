6960e94e5fe7bcb25c41eaf9b148d121b2d410a7
barakis <lokislayer@gmail.com>
2023-11-08 00:34:03 -0600

    Completed (#29)
    * Inital Commit.
    
    * Updated Text.
    
    * Fixed the validation to using regex
    
    * Fixed where it needs pascal case
    
    ---------
    
    Co-authored-by: Matthew J Brown <matthew@lokislayer.com>

---
+++ Phonebook.Barakisbrown/PhoneBook/PhoneBook.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using PhoneBook.Console.Menu;
+using Spectre.Console;
+
+MainMenu main = new();
+main.MainLoop();
+
+AnsiConsole.WriteLine("Thank you for using the Phone Book App with EF Core and SqlServer");
