5e8aad6e513b49ca07e4edda9ceba4fdf1efec38
Stan Choi <sjc591@nyu.edu>
2023-11-26 01:03:41 -0500

    Test commit to see if codacy will run

--- Phonebook.StanimalTheMan/Phonebook.StanimalTheMan/Program.cs
+++ Phonebook.StanimalTheMan/Phonebook.StanimalTheMan/Program.cs
@@ -5,3 +5,4 @@ context.Database.EnsureDeleted();
 context.Database.EnsureCreated();
 
 UserInterface.MainMenu();
+// codacy wouldn't run, maybe make another commit after opening folder containing everyone's solutions...
