fdf654d964d5f8cb57baff0c0f3e787aaedf2189
Stan Choi <43020892+StanimalTheMan@users.noreply.github.com>
2023-11-28 18:25:46 -0500

    PhoneBook basic requirements (#31)
    * Initialize console app and make models dir with contact entity
    
    * Implement services, utils, models, controllers, enums in order to CREATE a basic contact entity
    
    * View all contacts
    
    * View a contact, set up GetContactOptionInput method for delete and update operations todo
    
    * Delete contact
    
    * Add basic update functionality
    
    * Handle no contacts available scenario naively for now
    
    * Test commit to see if codacy will run

---
+++ Phonebook.StanimalTheMan/Phonebook.StanimalTheMan/Enums.cs
@@ -0,0 +1,14 @@
+﻿namespace Phonebook.StanimalTheMan;
+
+internal class Enums
+{
+    internal enum MenuOptions
+    {
+        AddContact,
+        DeleteContact,
+        UpdateContact,
+        ViewAllContacts,
+        ViewContact,
+        Quit
+    }
+}
---
+++ Phonebook.StanimalTheMan/Phonebook.StanimalTheMan/Program.cs
@@ -0,0 +1,8 @@
+﻿using Phonebook.StanimalTheMan;
+
+var context = new ContactsContext();
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
+
+UserInterface.MainMenu();
+// codacy wouldn't run, maybe make another commit after opening folder containing everyone's solutions...
