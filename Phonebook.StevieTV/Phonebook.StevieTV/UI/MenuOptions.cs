a74c61932f89a9424c51b6045cc9d9bd687788fd
Stevie Robinson <stevie.robinson@gmail.com>
2024-01-17 05:01:06 +0100

    completed PhoneBook - StevieTV (#34)
    * Initial setup of database
    
    * Basic Add and View Functions
    
    * Delete function
    
    * add email validation
    
    * add phone number validation and break out inputs into own methods
    
    * added update functionality

---
+++ Phonebook.StevieTV/Phonebook.StevieTV/UI/MenuOptions.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel;
+
+namespace Phonebook.StevieTV.UI;
+
+public enum MainMenuOptions
+{
+    [Description("View Contacts")]
+    ViewContacts,
+    [Description("Add a Contact")]
+    AddContact,
+    [Description("Delete a Contact")]
+    DeleteContact,
+    [Description("Update a Contact")]
+    UpdateContact,
+    [Description("Exit")]
+    Exit
+}
+
+public enum UpdateContactOptions
+{
+    Name,
+    Email,
+    Phone,
+    Save
+}
