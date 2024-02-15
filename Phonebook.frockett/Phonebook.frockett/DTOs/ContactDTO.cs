880e2a36c634e4df2e94fd1c02227f0db83c95b7
Crockett Ford <119839633+frockett@users.noreply.github.com>
2024-02-16 07:01:01 +0800

    submitting for review (#38)
    * initial commit, nuget packages installed
    
    * Added domain models, DTOs, and a mapper class to map them to each other
    
    * implemented repository, working on menu flow/logic before building out service layer
    
    * wrapping up for the night, continue work on UI
    
    * building out menus, moving input gathering methods to HandleUserInput class
    
    * menu mostly done, working on service layer
    
    * core functions done, time to refactor
    
    * pass bools as status codes up from repository. Some refactoring. Initial submission to codacy
    
    * some codacy suggestions
    
    * Deleted unused methods that create id to sequence index map in dictionary, since DTOs now contain id property
    
    * requested change made; editing contact now correctly replaces name and email

---
+++ Phonebook.frockett/Phonebook.frockett/DTOs/ContactDTO.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Phonebook.frockett.DTOs;
+
+public class ContactDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public string? ContactGroupName { get; set; }
+}
---
+++ Phonebook.frockett/Phonebook.frockett/Models/ContactGroup.cs
@@ -0,0 +1,9 @@
+﻿
+namespace Phonebook.frockett.Models;
+
+public class ContactGroup
+{
+    public int ContactGroupId { get; set; }
+    public string Name { get; set; }
+    public List<Contact>? Contacts { get; set; }
+}
