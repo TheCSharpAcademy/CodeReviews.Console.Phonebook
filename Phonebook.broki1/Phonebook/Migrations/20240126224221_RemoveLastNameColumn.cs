16882f3a9386f4e4d8ddfbaf9a2c5af7d41f7b7c
Justice Engle <justice.engle@icloud.com>
2024-02-22 20:01:49 -0500

    finished Phonebook proj

---
+++ Phonebook.broki1/Phonebook/Migrations/20240126224221_RemoveLastNameColumn.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phonebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveLastNameColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Contacts");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Contacts",
+                newName: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Contacts",
+                newName: "LastName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Contacts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Phonebook.broki1/Phonebook/Model/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Phonebook.Model;
+
+internal class Contact
+{
+
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string PhoneNumber { get; set; } = null!;
+
+}
