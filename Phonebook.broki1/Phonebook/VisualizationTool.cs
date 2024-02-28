01844d8813d4b03bbb7950b698e6109df2cf6ed0
broki1 <justice.engle@icloud.com>
2024-02-28 14:37:00 -0500

    finished Phonebook proj (#39)

---
+++ Phonebook.broki1/Phonebook/VisualizationTool.cs
@@ -0,0 +1,15 @@
+﻿using ConsoleTableExt;
+using Phonebook.Model;
+
+namespace Phonebook;
+
+internal class VisualizationTool
+{
+
+    internal static void PrintContacts(List<Contact> contacts)
+    {
+        Console.WriteLine();
+        ConsoleTableBuilder.From(contacts).WithTitle("CONTACTS").WithFormat(ConsoleTableBuilderFormat.Alternative).ExportAndWriteLine();
+    }
+
+}
