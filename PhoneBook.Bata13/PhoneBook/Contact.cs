8618b4ec560d8b14f005f2693fa91c707aed9399
Jinboi <65874606+Jinboi@users.noreply.github.com>
2024-03-23 23:23:32 -0700

    updated as per changes requested

--- PhoneBook.Bata13/PhoneBook/Contact.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PhoneBook;
-internal class Contact
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Email { get; set; }
-    public string PhoneNumber { get; set; }
-}
