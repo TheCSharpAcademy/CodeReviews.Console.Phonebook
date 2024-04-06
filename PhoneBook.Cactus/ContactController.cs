c7b67d7f4e66cf98eb627fdc318f75eef60fcacd
Jingwen Xia <amyfor7@gmail.com>
2024-04-06 21:23:37 +0800

    feat: init

---
+++ PhoneBook.Cactus/ContactController.cs
@@ -0,0 +1,29 @@
+﻿namespace PhoneBook.Cactus;
+public class ContactController
+{
+    public static void AddContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void DeleteContact()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void GetContactById()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void GetContacts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public static void UpdateContact()
+    {
+        throw new NotImplementedException();
+    }
+}
+
---
+++ PhoneBook.Cactus/Program.cs
@@ -0,0 +1,50 @@
+﻿using PhoneBook.Cactus;
+using Spectre.Console;
+enum MenuOptions
+{
+    AddContact,
+    DeleteContact,
+    UpdateContact,
+    ViewContact,
+    ViewAllContact,
+    Quit
+}
+
+public static class Application
+{
+    public static void Main()
+    {
+        bool isAppRunning = true;
+        while (isAppRunning)
+        {
+            var option = AnsiConsole.Prompt(
+            new SelectionPrompt<MenuOptions>()
+            .Title("What would you like to do?")
+            .AddChoices(
+                MenuOptions.AddContact,
+                MenuOptions.DeleteContact,
+                MenuOptions.UpdateContact,
+                MenuOptions.ViewAllContact,
+                MenuOptions.ViewContact));
+
+            switch (option)
+            {
+                case MenuOptions.AddContact:
+                    ContactController.AddContact();
+                    break;
+                case MenuOptions.DeleteContact:
+                    ContactController.DeleteContact();
+                    break;
+                case MenuOptions.UpdateContact:
+                    ContactController.UpdateContact();
+                    break;
+                case MenuOptions.ViewContact:
+                    ContactController.GetContactById();
+                    break;
+                case MenuOptions.ViewAllContact:
+                    ContactController.GetContacts();
+                    break;
+            }
+        }
+    }
+}
