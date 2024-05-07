5098441f2bfa63fe6f29c3a11c7412bc41151970
Samuel <Sam.Gannon@sportssouth.biz>
2024-05-07 11:28:53 -0500

    // streamedline validation for mailaddress

--- Phonebook.samggannon/Phonebook.samggannon/Utilities/Validation.cs
+++ Phonebook.samggannon/Phonebook.samggannon/Utilities/Validation.cs
@@ -16,43 +16,15 @@ internal static class Validation
 
         try
         {
-            // Normalize the domain
-            emailAddress = Regex.Replace(emailAddress, @"(@)(.+)$", DomainMapper,
-                                  RegexOptions.None, TimeSpan.FromMilliseconds(200));
-
-            // Examines the domain part of the email and normalizes it.
-            string DomainMapper(Match match)
-            {
-                // Use IdnMapping class to convert Unicode domain names.
-                var idn = new IdnMapping();
-
-                // Pull out and process domain name (throws ArgumentException on invalid)
-                string domainName = idn.GetAscii(match.Groups[2].Value);
-
-                return match.Groups[1].Value + domainName;
-            }
+            var mailAddress = new MailAddress(emailAddress);
+            return true;
         }
-        catch (RegexMatchTimeoutException e)
-        {
-            return false;
-        }
-        catch (ArgumentException e)
-        {
-            return false;
-        }
-
-        try
-        {
-            return Regex.IsMatch(emailAddress,
-                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
-                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
-        }
-        catch (RegexMatchTimeoutException)
+        catch (FormatException)
         {
             return false;
         }
     }
-
+        
 
     public static bool IsPhoneNumberValid(string phoneNumber)
     {
