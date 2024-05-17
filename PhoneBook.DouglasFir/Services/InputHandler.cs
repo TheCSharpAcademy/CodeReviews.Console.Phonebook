3b2e7a36d40a194ae85cff88639071781825d41d
rankdjr <douglas.rankjr@gmail.com>
2024-05-16 22:22:33 -0700

    setup very basic input handling

---
+++ PhoneBook.DouglasFir/Services/InputHandler.cs
@@ -0,0 +1,24 @@
+﻿using Spectre.Console;
+
+namespace PhoneBook.DouglasFir.Services;
+
+public class InputHandler
+{
+    public void PauseForContinueInput()
+    {
+        AnsiConsole.WriteLine("Press any key to continue...");
+        Console.ReadKey();
+    }
+
+    public bool ConfirmAction(string actionPromptMessage)
+    {
+        if (!AnsiConsole.Confirm(actionPromptMessage))
+        {
+            Utilities.DisplayCancellationMessage("Operation cancelled.");
+            PauseForContinueInput();
+            return false;
+        }
+
+        return true;
+    }
+}
