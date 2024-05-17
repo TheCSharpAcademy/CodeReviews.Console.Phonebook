73c96a8ce8a2159c31cbf59a364ab1641251780f
rankdjr <douglas.rankjr@gmail.com>
2024-05-16 22:25:32 -0700

    added model for contact categories

---
+++ PhoneBook.DouglasFir/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace PhoneBook.DouglasFir.Models;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Contact> Contacts { get; set; } = new List<Contact>();
+}
