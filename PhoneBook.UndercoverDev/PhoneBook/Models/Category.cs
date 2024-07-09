04fd85323e044bdc327d0d3f68406698289840a2
N-Endy <okafornel@gmail.com>
2024-07-09 23:40:59 +0100

    Add models for contact and category

--- PhoneBook.UndercoverDev/PhoneBook/Models/Category.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Models/Category.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Models
 {
     public class Category
     {
-        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public ICollection<Contact> Contacts { get; set; }
     }
 }
--- PhoneBook.UndercoverDev/PhoneBook/Models/Contact.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Models/Contact.cs
@@ -1,7 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PhoneBook.Models
 {
     public class Contact
     {
-        
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
     }
 }
