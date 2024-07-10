79542e757e81923fa8296854cf5909a464f1cdda
N-Endy <okafornel@gmail.com>
2024-07-10 11:29:55 +0100

    Configure connection string for SQL Server

--- PhoneBook.UndercoverDev/PhoneBook/Models/ContactContext.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Models/ContactContext.cs
@@ -1,7 +1,20 @@
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
 namespace PhoneBook.Models
 {
-    public class ContactContext
+    public class ContactContext : DbContext
     {
-        
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Retrieve the connection string
+            var connectionString = ConfigurationManager.ConnectionStrings["ContactContext"].ConnectionString;
+
+            // Set the connection string
+            optionsBuilder.UseSqlServer(connectionString);
+        }
     }
 }
