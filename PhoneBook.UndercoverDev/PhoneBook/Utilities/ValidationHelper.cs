632e8a776e2e048198361c11f22ec3b20da8b80d
N-Endy <okafornel@gmail.com>
2024-07-11 09:49:48 +0100

    Add validation for checking if category name exists

---
+++ PhoneBook.UndercoverDev/PhoneBook/Utilities/ValidationHelper.cs
@@ -0,0 +1,10 @@
+using PhoneBook.Models;
+
+namespace PhoneBook.Utilities
+{
+    public class ValidationHelper
+    {
+        internal static bool CategoryExists(ContactContext context, string categoryName)
+            => context.Categories.Any(c => c.Name == categoryName);
+    }
+}
