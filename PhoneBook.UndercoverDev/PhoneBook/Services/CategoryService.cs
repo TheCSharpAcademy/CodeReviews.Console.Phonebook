86f59cabfaf1e3620ddb7d81bb64e28307a81082
N-Endy <okafornel@gmail.com>
2024-07-11 09:31:06 +0100

    Add code to view categories

--- PhoneBook.UndercoverDev/PhoneBook/Services/CategoryService.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Services/CategoryService.cs
@@ -1,4 +1,7 @@
 using PhoneBook.Controllers;
+using PhoneBook.Models;
+using PhoneBook.Views;
+using Spectre.Console;
 
 namespace PhoneBook.Services
 {
@@ -12,7 +15,32 @@ namespace PhoneBook.Services
 
         internal static void ViewCategories()
         {
-            throw new NotImplementedException();
+            var categories = CategoryController.GetCategories();
+            CategoryView.DisplayCategories(categories);
+            MainMenu.ShowMainMenu();
+        }
+
+        internal static void ViewContactsInCategories()
+        {
+            var category = GetCategoriesOptionInput();
+            UserInteraction.UserInteractions.DisplayContacts(category);
+            MainMenu.ShowMainMenu();
+        }
+
+        internal static Category GetCategoriesOptionInput()
+        {
+            var categories = CategoryController.GetCategories();
+            var categoriesArray = categories.Select(c => c.Name).ToArray();
+            var option = AnsiConsole.Prompt(new 
+                SelectionPrompt<string>()
+                .Title("\n[bold][blue]Select a Category to View Contacts[/][/]")
+                .AddChoices(categoriesArray)
+            );
+
+            var id = categories.Single(c => c.Name == option).CategoryId;
+            var category = CategoryController.GetCategoryById(id);
+
+            return category;
         }
     }
 }
