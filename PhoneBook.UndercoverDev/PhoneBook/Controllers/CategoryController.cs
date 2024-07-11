c1a700c3f83a9951270b66cd2fbccabf43928932
N-Endy <okafornel@gmail.com>
2024-07-11 09:29:37 +0100

    Add code to fetch categories

--- PhoneBook.UndercoverDev/PhoneBook/Controllers/CategoryController.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Controllers/CategoryController.cs
@@ -10,5 +10,20 @@ namespace PhoneBook.Controllers
             context.Add(category);
             context.SaveChanges();
         }
+
+        internal static List<Category> GetCategories()
+        {
+            using var context = new ContactContext();
+            var categories = context.Categories.ToList();
+
+            return categories;
+        }
+
+        internal static Category GetCategoryById(int id)
+        {
+            using var context = new ContactContext();
+            var category = context.Categories.Single(c => c.CategoryId == id);
+            return category;
+        }
     }
 }
