dfa46c6dc903a29345178e4e5cdd63b724ece4e5
N-Endy <okafornel@gmail.com>
2024-07-12 13:56:43 +0100

    Add code to view all contacts

--- PhoneBook.UndercoverDev/PhoneBook/Views/ContactView.cs
+++ PhoneBook.UndercoverDev/PhoneBook/Views/ContactView.cs
@@ -1,7 +1,31 @@
+
+using PhoneBook.Models;
+using Spectre.Console;
+
 namespace PhoneBook.Views
 {
     public class ContactView
     {
-        
+        private static readonly string[] columns = ["Id", "Name", "Phone", "Email"];
+
+        internal static void DisplayContacts(List<Contact> contacts)
+        {
+            var grid = new Grid()
+                .AddColumn()
+                .AddColumn()
+                .AddColumn()
+                .AddColumn();
+            
+            grid.AddRow(columns);
+
+            var count = 1;
+            foreach (var contact in contacts)
+            {
+                grid.AddRow(count.ToString(), contact.Name, contact.PhoneNumber, contact.Email);
+                count++;
+            }
+
+            AnsiConsole.Write(grid);
+        }
     }
 }
