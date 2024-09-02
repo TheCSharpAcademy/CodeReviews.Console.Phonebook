3b5f51aa38885474ac7ef685b02cea30e2c89ff2
Thomas Henley <thomas.e.henley@gmail.com>
2024-09-01 22:47:41 -0400

    initial db connectivity

---
+++ Phonebook.thomas-henley/PhoneBook/PhoneBook/Contact.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace PhoneBook;
+
+public class PhonebookDbContext : DbContext
+{
+    private readonly string _cs;
+    public DbSet<Contact> Contacts { get; set; }
+
+    public PhonebookDbContext(string cs)
+    {
+        _cs = cs;
+    }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(_cs);
+    }
+}
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+}
---
+++ Phonebook.thomas-henley/PhoneBook/PhoneBook/Program.cs
@@ -0,0 +1,21 @@
+﻿using PhoneBook;
+
+var cxString = "Server=(localdb)\\MSSQLLocalDB;Database=HenleyPhonebookDB;Integrated Security=true";
+
+var db = new PhonebookDbContext(cxString);
+
+db.Database.EnsureCreated();
+
+db.Contacts.Add(
+    new Contact()
+    {
+        Name = "Thomas Henley",
+        Email = "thomas.e.henley@gmail.com",
+        Phone = "0123456789",
+    });
+
+db.SaveChanges();
+
+db.Database.EnsureDeleted();
+
+db.Dispose();
