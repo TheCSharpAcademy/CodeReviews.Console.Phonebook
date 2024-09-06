685c20fe3851742bbfb5a8079576843e407a947f
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-06 18:20:13 -0400

    Add entity repository

--- Phonebook.tonyissa/Repositories/PhonebookRepository.cs
+++ Phonebook.tonyissa/Repositories/PhonebookRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Phonebook.tonyissa.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,3 +8,43 @@ using System.Threading.Tasks;
 
 namespace Phonebook.tonyissa.Repositories;
 
+public class PhonebookRepository
+{
+    private readonly PhonebookContext _context;
+
+    public PhonebookRepository(PhonebookContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddEntryAsync(Contact contact)
+    {
+        await _context.Contacts.AddAsync(contact);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<Contact> GetEntryAsync(int id)
+    {
+        return await _context.Contacts.FindAsync(id);
+    }
+
+    public async Task<List<Contact>> GetAllEntriesAsync()
+    {
+        return await _context.Contacts.ToListAsync<Contact>();
+    }
+
+    public async Task UpdateEntryAsync(Contact entry)
+    {
+        _context.Contacts.Update(entry);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteEntryAsync(int id)
+    {
+        var entry = await _context.Contacts.FindAsync(id);
+
+        if (entry == null) return;
+
+        _context.Contacts.Remove(entry);
+    }
+}
