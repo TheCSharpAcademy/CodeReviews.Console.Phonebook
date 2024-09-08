b27ad2febb4de99c812a15024c524d3c91f52c61
tonyissa <52683702+tonyissa@users.noreply.github.com>
2024-09-08 16:15:12 -0400

    Add migrations, services class and restructure project

---
+++ Phonebook.tonyissa/Services/PhonebookService.cs
@@ -0,0 +1,13 @@
+﻿using Phonebook.tonyissa.Context;
+using Phonebook.tonyissa.Repositories;
+using Phonebook.tonyissa.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phonebook.tonyissa.Services;
+public static class PhonebookService
+{
+}
