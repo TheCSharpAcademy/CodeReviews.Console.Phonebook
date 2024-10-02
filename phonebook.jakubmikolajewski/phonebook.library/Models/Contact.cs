798da3b68987f295694d6a9c37088c7567d8c3d5
jakubmikolajewski <137167332+jakubmikolajewski@users.noreply.github.com>
2024-10-02 21:13:18 +0200

    phonebook project for review (#79)

---
+++ phonebook.jakubmikolajewski/phonebook.library/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Phonebook.Library.Models;
+public class Contact
+{
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+    [AllowNull]
+    public string Category { get; set; } = "Other";
+}
