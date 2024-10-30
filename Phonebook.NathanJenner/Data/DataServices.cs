791c330a94a8202d1cc1655d96a2effcdaa564e5
NathanJenner <nathanj@steadfasttech.com.au>
2024-10-30 13:23:06 +1100

    Code Review Request.
    Code Review Request.
    thanks.

---
+++ Phonebook.NathanJenner/Data/DataServices.cs
@@ -0,0 +1,31 @@
+﻿using Console.Phonebook.App.Entities;
+
+namespace Console.Phonebook.App.Data;
+
+public class DataServices
+{
+    public ApplicationDbContext appDbContext = new();
+
+    public static void CreateDatabaseIfNotExists()
+    {
+
+    }
+
+    public void PostContact(Contact contact)
+    {
+        appDbContext.Contacts.Add(contact);
+        appDbContext.SaveChanges();
+    }
+
+    public List<Contact> GetAllContacts()
+    {
+        List<Contact> contacts = appDbContext.Contacts.ToList();
+        return contacts;
+    }
+
+    public void DeleteSelectedContact(Contact contact)
+    {
+        appDbContext.Remove(contact);
+        appDbContext.SaveChanges();
+    }
+}
---
+++ Phonebook.NathanJenner/Services/ValidationServices.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+
+namespace Console.Phonebook.App.Services;
+
+internal class ValidationServices
+{
+    public static bool IsValidEmailAddress(string inputEmail)
+    {
+        var email = new EmailAddressAttribute();
+        return email.IsValid(inputEmail);
+    }
+
+    public static bool IsValidPhoneNumber(string phoneNumber)
+    {
+        Regex numberRegex = new Regex(@"^([0-9]{9})$");
+        Regex phoneRegex = new Regex(@"^([0-9]{4}\s[0-9]{3}\s[0-9]{3})$");
+
+        if (numberRegex.IsMatch(phoneNumber) || phoneRegex.IsMatch(phoneNumber))
+        {
+            return true;
+        }
+        return false;
+    }
+}
