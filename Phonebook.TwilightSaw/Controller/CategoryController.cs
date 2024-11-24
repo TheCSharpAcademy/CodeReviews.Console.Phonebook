1b6eddf1ea6541207025d90a6cba1981418155d2
TwilightSaw <56154417+TwilightSaw@users.noreply.github.com>
2024-11-24 17:15:30 +0100

    Phonebook 0.04a
    Added all files

---
+++ Phonebook.TwilightSaw/Controller/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TwilightSaw.Phonebook.Model;
+
+namespace TwilightSaw.Phonebook.Controller;
+
+public class CategoryController(AppDbContext context)
+{
+    public void Create(Category category)
+    {
+        context.Categories.Add(category);
+        context.SaveChanges();
+    }
+
+    public List<Category> Read()
+    {
+        return context.Categories.Select(t => t).ToList();
+    }
+
+    public void Delete(Category category)
+    {
+        context.Categories.Where(c => c.Id.Equals(category.Id)).AsNoTracking().ExecuteDelete();
+        context.SaveChanges();
+    }
+
+    public void Update(Category category, string name)
+    {
+        context.Categories.Where(c => c.Id.Equals(category.Id)).AsNoTracking().
+            ExecuteUpdate(s => s.SetProperty(c => c.Name, c => name));
+        context.SaveChanges();
+        context.Entry(category).Reload();
+    }
+}
---
+++ Phonebook.TwilightSaw/Helpers/Validation.cs
@@ -0,0 +1,44 @@
+﻿using Spectre.Console;
+
+namespace TwilightSaw.Phonebook.Helpers;
+
+public class Validation
+{
+    public static string Validate(Action action, bool getMessage)
+    {
+        try
+        {
+            action();
+        }
+        catch (Exception e)
+        {
+            return getMessage ? e.Message : "";
+        }
+        return "Executed successfully";
+    }
+
+    public static string Validate<T>(T action, bool getMessage, out T back)
+    {
+        try
+        {
+            back = action;
+        }
+        catch (Exception e)
+        {
+            back = default;
+            return getMessage ? e.Message : "";
+        }
+        return "Executed successfully";
+    }
+
+    public static void EndMessage(string? message)
+    {
+        if (message != null)
+        {
+            AnsiConsole.MarkupLine($"[olive]{message}[/]");
+            AnsiConsole.Markup($"[grey]Press any key to continue.[/]");
+            Console.ReadKey(intercept: true);
+        }
+        Console.Clear();
+    }
+}
