f5f38950d39724e9fdda625c1a96ae9f25a61dc4
Albina Diakova <256982@student.usn.no>
2025-01-08 14:24:10 +0100

    Change PhoneBookContext, delete second-migration

--- PhoneBook.Bina28/PhoneBookContext.cs
+++ PhoneBook.Bina28/PhoneBookContext.cs
@@ -1,12 +1,28 @@
 ﻿namespace PhoneBook.Bina28;
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 internal class PhoneBookContext : DbContext
 {
+	string _connectionString = string.Empty;
+
+	public PhoneBookContext()
+	{
+		var config = new ConfigurationBuilder()
+			.AddJsonFile("appsettings.json", false, true)
+			.Build();
+
+		_connectionString = config.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found");
+
+	}
+
 	public DbSet<PhoneBook> PhoneBooks { get; set; }
 
-	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+	{
+		optionsBuilder.UseSqlServer(_connectionString);
+	}
 
-		optionsBuilder.UseSqlServer($"Server=(localdb)\\LocalDBDemo;Database=DB;Trusted_Connection=True;");
+
 }
